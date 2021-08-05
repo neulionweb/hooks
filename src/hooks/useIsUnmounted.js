730b4dd1a6f82eb8e3d993a5b2459e695df486ea
Wayne Mao <wayne.mao@neulion.com.cn>
2021-08-05 23:46:31 +0800

    introduce prettier

--- src/hooks/useIsUnmounted.js
+++ src/hooks/useIsUnmounted.js
@@ -1,12 +1,15 @@
-import {useEffect, useRef} from 'react';
+import { useEffect, useRef } from 'react';
 
 const useIsUnmounted = () => {
-	const unmountedRef = useRef(false);
-	useEffect(() => () => {
-			unmountedRef.current = true;
-	}, []);
+  const unmountedRef = useRef(false);
+  useEffect(
+    () => () => {
+      unmountedRef.current = true;
+    },
+    []
+  );
 
-	return unmountedRef?.current === true;
+  return unmountedRef?.current === true;
 };
 
 export default useIsUnmounted;
--- src/hooks/useWindowResize.js
+++ src/hooks/useWindowResize.js
@@ -1,29 +1,32 @@
-import {useCallback, useState, useEffect} from 'react';
+import { useCallback, useState, useEffect } from 'react';
 
 function useWindowSize() {
-	const [isClient] = useState(typeof window === 'object');
+  const [isClient] = useState(typeof window === 'object');
 
-	const getSizeCallback = useCallback(() => ({
-		width: isClient ? window.innerWidth : undefined,
-		height: isClient ? window.innerHeight : undefined,
-	}), [isClient]);
+  const getSizeCallback = useCallback(
+    () => ({
+      width: isClient ? window.innerWidth : undefined,
+      height: isClient ? window.innerHeight : undefined,
+    }),
+    [isClient]
+  );
 
-	const [windowSize, setWindowSize] = useState(getSizeCallback());
+  const [windowSize, setWindowSize] = useState(getSizeCallback());
 
-	useEffect(() => {
-		if (!isClient) {
-			return false;
-		}
+  useEffect(() => {
+    if (!isClient) {
+      return false;
+    }
 
-		function handleResize() {
-			setWindowSize(getSizeCallback());
-		}
+    function handleResize() {
+      setWindowSize(getSizeCallback());
+    }
 
-		window.addEventListener('resize', handleResize);
-		return () => window.removeEventListener('resize', handleResize);
-	}, [getSizeCallback, isClient]);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [getSizeCallback, isClient]);
 
-	return windowSize;
+  return windowSize;
 }
 
 export default useWindowSize;
